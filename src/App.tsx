8a39d8136d35c506d3e82393afd1f39d14af8d8f
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-06-01 11:51:12 +0600

    create project; create dirrectories:components,lib; crete and config .editorconfig

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+import './App.css';
+
+function App() {
+    const [order, setOrder] = useState([]);
+
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default App;
